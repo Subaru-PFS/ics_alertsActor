29eb60e34df923e5968a18b72ef6d27ab5a20c06
Craig Loomis <cloomis@astro.princeton.edu>
2018-03-22 23:24:54 +0000

    Add mechanism to register actor.keyword.idx to be forwarded to STS
    The config/STS.yaml file lists the bindings.

--- python/alertsActor/Controllers/actorRules.py
+++ python/alertsActor/Controllers/actorRules.py
@@ -1,9 +1,52 @@
 import os
+import time
+import functools
+
 import yaml
 
 import logging
-# from .STS import STSlib
+from STSpy import radio, datum
 
+class STSCallback(object):
+    def __init__(self, actorName, stsMap, actor, logger):
+        self.actorName = actorName
+        self.stsMap = stsMap
+        self.actor = actor
+        self.logger = logger
+
+    def keyToStsTypeAndValue(self, key):
+        """ Return the STS type for theActor given key. """
+
+        if isinstance(key, float):
+            return datum.Datum.FloatWithText, float(key)
+        elif isinstance(key, int):
+            return datum.Datum.IntegerWithText, int(key)
+        elif isinstance(key, str):
+            return datum.Datum.Text, str(key)
+        else:
+            raise TypeError('do not know how to convert a %s' % (key))
+        
+    def __call__(self, key):
+        """ This function is called when new keys are received by the dispatcher. """
+        
+        toSend = []
+        now = int(time.time())
+        for f_i, f in enumerate(self.stsMap):
+            keyFieldId, stsId = f
+            val = key[keyFieldId]
+
+            alertState = self.actor.getAlertState(key, keyFieldId)
+            stsType, val = self.keyToStsTypeAndValue(key[keyFieldId])
+            self.logger.debug('updating STSid %d(%s) from %s.%s[%s] with (%s, %s)',
+                              stsId, stsType,
+                              key.actor, key.name, keyFieldId,
+                              val, alertState)
+            toSend.append(stsType(stsId, timestamp=now, value=(val, alertState)))
+
+        self.logger.info('flushing STS, with: %s', toSend)
+        stsServer = radio.Radio()
+        stsServer.transmit(toSend)
+        
 class ActorRules(object):
     def __init__(self, actor, name):
         self.name = name
@@ -40,8 +83,12 @@ class ActorRules(object):
                     raise KeyError(f'keyvar {keyName} is not in the {self.name} model')
 
                 keyConfig = cfgActors[self.name][keyName]
-                self.logger.warn('loading STS key %s as %s', keyName, keyConfig)
-                stsId = keyConfig['stsId']
-                fields = keyConfig.get('keyField', None)
 
-                self.logger.warn('not wiring in %s.%s(%s) to %s', self.name, keyName, fields, stsId)
+                self.logger.warn('wiring in %s.%s to %s', self.name, keyName, keyConfig)
+
+                for cb in keyVar._callbacks:
+                    if cb.__class__.__name__ == STSCallback.__name__:
+                        self.logger.warn('removing callback: %s', cb)
+                        keyVar.removeCallback(cb)
+                keyVar.addCallback(STSCallback(self.name, keyConfig, self.actor, self.logger),
+                                   callNow=False)
