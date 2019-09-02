5f1e67c7635610cabddae1d0632be5512aaa0e97
Arnaud Le Fur <alefur@lam.fr>
2019-09-02 12:02:36 +0000

    add tests alert

---
+++ python/alertsActor/Controllers/tests.py
@@ -0,0 +1,25 @@
+from importlib import reload
+
+import alertsActor.Controllers.actorRules as actorRules
+from opscore.protocols import types
+
+reload(actorRules)
+
+
+def checkTempRange(cls, keyword):
+    alertState = "OK"
+    values = keyword.getValue(doRaise=False)
+    value = values[cls.ind] if isinstance(values, tuple) else values
+
+    if isinstance(value, types.Invalid):
+        return '{key}[{ind}] : is unknown'.format(**dict(key=keyword.name, ind=cls.ind))
+
+    if not 0 < value < 30:
+        alertState = '{key}[{ind}] : {value}C out of range'.format(**dict(key=keyword.name, ind=cls.ind, value=value))
+
+    return alertState
+
+
+class tests(actorRules.ActorRules):
+    def __init__(self, actor, name):
+        actorRules.ActorRules.__init__(self, actor, name)
