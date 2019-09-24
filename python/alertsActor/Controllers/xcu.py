f1af9a4c78f6641b64e9d79fe5673a993b8494f1
Arnaud Le Fur <arnaud.lefur@lam.fr>
2019-09-24 19:28:05 +0200

    limits are now loaded from config/cryoMode.yaml wrt cryoMode keyword

--- python/alertsActor/Controllers/xcu.py
+++ python/alertsActor/Controllers/xcu.py
@@ -1,12 +1,14 @@
+import os
 from importlib import reload
 
 import alertsActor.Controllers.actorRules as actorRules
+import yaml
 from opscore.protocols import types
 
 reload(actorRules)
 
 
-def checkTempRange(cls, keyword):
+def checkTempRange(cls, keyword, model):
     alertState = "OK"
     values = keyword.getValue(doRaise=False)
     value = values[cls.ind] if isinstance(values, tuple) else values
@@ -20,9 +22,22 @@ def checkTempRange(cls, keyword):
     return alertState
 
 
+def ionpumpState(cls, keyword, model):
+    alertState = "OK"
+    return alertState
+
+
 class xcu(actorRules.ActorRules):
     def __init__(self, actor, name):
         actorRules.ActorRules.__init__(self, actor, name)
 
     def getAlertConfig(self, name='xcu_{cam}'):
         return actorRules.ActorRules.getAlertConfig(self, name=name)
+
+    def loadCryoMode(self, mode):
+        camType = 'nir' if 'xcu_n' in self.name else 'vis'
+        with open(os.path.expandvars(f'$ICS_ALERTSACTOR_DIR/config/cryoMode.yaml'), 'r') as cfgFile:
+            cfg = yaml.load(cfgFile, Loader=yaml.FullLoader)
+
+        conf = cfg[camType][mode]
+        return conf if conf is not None else {}
